04f9b4b74b9c49900e3bda7c947a9fbb57deb7aa
Shixiong Qi <qishixiong19931009@gmail.com>
2021-03-27 20:01:44 -0700

    update maxmin model

--- py/DRFsim-100func-40nodes.py
+++ py/DRFsim-100func-40nodes.py
@@ -12,8 +12,9 @@ solverName = sys.argv[1]
 # Initialize input parameters
 total_ticks = 1
 
-l = 10 # total 40 nodes available
-n = 100 * l / 1 * 2  # total 4 functions 
+l = 40 # total 40 nodes available
+# n = 100 * l / 4 * 2  # total 4 functions 
+n = 100
 m = 2 # 2 kinds of resources
 
 func_1 = {'desiredPodCountSLO': 6, 'podCPUUsage': 5.0, 'podMemUsage': 1.0, 'functionName': "fairnessData_1"}
@@ -23,10 +24,30 @@ func_4 = {'desiredPodCountSLO': 2, 'podCPUUsage': 1.0, 'podMemUsage': 1.0, 'func
 func_null = {'desiredPodCountSLO': 0, 'podCPUUsage': 0.01, 'podMemUsage': 0.01, 'functionName': "fairnessData_null"}
 # func_null = {'desiredPodCountSLO': 0, 'podCPUUsage': 0, 'podMemUsage': 0, 'functionName': "fairnessData_null"}
 
-node_1 = {'CPUCapacity': 25, 'MemCapacity': 10}
-node_2 = {'CPUCapacity': 10, 'MemCapacity': 25}
+if n == 100:
+    scale = 1 # 100 functions
+elif n == 200:
+    scale = 2 # 200 functions
+elif n == 400:
+    scale = 4
+elif n == 800:
+    scale = 8
+elif n == 1600:
+    scale = 16
+elif n == 3200:
+    scale = 32
+elif n == 6400:
+    scale = 64 # 6400 functions
+else:
+    scale = 1
+    exit("n is not allowed")
 
-node = [node_1, node_2] * int(l/2)
+node_1 = {'CPUCapacity': 25 * scale, 'MemCapacity': 10 * scale}
+node_2 = {'CPUCapacity': 10 * scale, 'MemCapacity': 25 * scale}
+cpu = node_1['CPUCapacity'] + node_2['CPUCapacity']
+mem = node_1['MemCapacity'] + node_2['MemCapacity']
+
+node = [node_1, node_2] * int(l / 2)
 
 delta = []
 for clk in range(total_ticks):
@@ -38,16 +59,24 @@ for clk in range(total_ticks):
         output  = LP_Models(func, l, n, m, node, solverName)
         delta.append((time.time() - tick) * 1000000)
         # print("Execution time of {}: {} us".format(solverName, (time.time() - tick) * 1000000))
+        cpu_lp_alloc = [0] * len(output) 
+        mem_lp_alloc = [0] * len(output)
+        for i in range(len(output)):
+            cpu_lp_alloc[i] = output[i] * func[i]['podCPUUsage']
+            mem_lp_alloc[i] = output[i] * func[i]['podMemUsage']
         # print("[t = {}] placed_pods_list: {}".format(clk, output))
+        # print("[t = {}] cpu_lp_alloc: {} \tmem_lp_alloc: {}".format(clk, cpu_lp_alloc, mem_lp_alloc))
     elif solverName == 'maxmin':
-        # output = maxmin_Models(func, l, n, m, node_1, node_2)
-        cpu_maxmin_alloc, mem_maxmin_alloc = maxmin_Models(func, l, n, m, node_1, node_2)
-        print("[t = {}] cpu_maxmin_alloc: {} \tmem_maxmin_alloc: {}".format(clk, cpu_maxmin_alloc, mem_maxmin_alloc))
+        tick = time.time()
+        cpu_maxmin_alloc, mem_maxmin_alloc = maxmin_Models(func, l, n, m, node)
+        delta.append((time.time() - tick) * 1000000)
+        # print("[t = {}] cpu_maxmin_alloc: {} \tmem_maxmin_alloc: {}".format(clk, cpu_maxmin_alloc, mem_maxmin_alloc))
     elif solverName[:3] == 'drf':
         # print("DRF algorithm")
         # print("\n============== {} cycle =============".format(clk))
         tick = time.time()
-        cpu_drf_alloc, mem_drf_alloc = DRF_Var(func, node, 35*l/2, 35*l/2, solverName)
+        # cpu_drf_alloc, mem_drf_alloc = DRF_Var(func, node, (25 + 10) * l / 2, (10 + 25) * l / 2, solverName)
+        cpu_drf_alloc, mem_drf_alloc = DRF_Var(func, node, cpu * l / 2, mem * l / 2, solverName)
         delta.append((time.time() - tick) * 1000000)
         # print("Execution time of {}: {} us".format(solverName, (time.time() - tick) * 1000000))
         # exit("Stop at the 1st cycle")
@@ -60,4 +89,4 @@ for clk in range(total_ticks):
             ''')
         exit(1)
     # print("[t = {}] placed_pods_list: {}".format(clk, output))
-print("Average completion time (us): ", sum(delta)/len(delta))
+print("Average completion time (milisecond): ", sum(delta)/len(delta))
--- py/maxmin_model_def.py
+++ py/maxmin_model_def.py
@@ -7,54 +7,11 @@ from math import gcd
 import numpy as np
 import time
 
-def workload_Init(func, l, n, m, node_1, node_2):
-    nr = []
-    resource_needed = []
-    for i in range(n):
-        nr.append(func[i]['desiredPodCountSLO'])
-        resource_needed.append([func[i]['podCPUUsage'], func[i]['podMemUsage']])
-    # print("required pods number by each functon:", nr)
-    # print("pod resource requested by each function is ", resource_needed)
-    total_requested_pods = sum(nr)
-    a = [([resource_needed[i]] *(nr[i])) for i in range(n)] # Initialize the resource request of each Pod
-    # print("pod resource requested for each function is ", a)
-
-    c = list(zip([node_1['CPUCapacity'], node_2['CPUCapacity']], [node_1['MemCapacity'], node_2['MemCapacity']]))# combine cpu and memory to initailize the resource capacity of each node
-    # print("resource on each node is ", c)
-
-    c_cpu_total = 0
-    c_mem_total = 0
+def workload_Init(l, node):
+    c = []
     for i in range(l):
-        c_cpu_total += c[i][0]
-        c_mem_total += c[i][1]
-    # print("total cpu is: ", c_cpu_total)
-    # print("total mem is: ", c_mem_total)
-    total_resource = [c_cpu_total, c_mem_total]
-    dominant_resource = [] # first is the amount of required dominant resource, second is the total resource for that type of resource
-    dominant_resource_flags = [] # 0 is cpu, 1 is memory
-    minimum_dominant_resource = [resource_needed[0][0], c_cpu_total] # first is the amount of required dominant resource, second is total resource for that type of resource
-    nodes_remainings = c[:]
-
-    for i in range(n): # calculate the dominant resource for each function
-        cpu_share = resource_needed[i][0]/c_cpu_total
-        mem_share = resource_needed[i][1]/c_mem_total
-        if (cpu_share >= mem_share):
-            dominant_resource_flags.append(0)
-            dominant_resource.append([resource_needed[i][0], c_cpu_total])    
-            if minimum_dominant_resource[0]/minimum_dominant_resource[1] > cpu_share:
-                minimum_dominant_resource = [resource_needed[i][0], c_cpu_total]
-        else:
-            dominant_resource_flags.append(1)
-            dominant_resource.append([resource_needed[i][1], c_mem_total])
-            if minimum_dominant_resource[0]/minimum_dominant_resource[1] > mem_share:
-                minimum_dominant_resource = [resource_needed[i][1], c_mem_total]
-
-    # Calcuate dr for each function
-    required_uniform_share = []
-    for i in range(n):
-        required_uniform_share.append((dominant_resource[i][0] * minimum_dominant_resource[1])/(dominant_resource[i][1] * minimum_dominant_resource[0]))
-
-    return a, nr, c, resource_needed, dominant_resource, total_resource, required_uniform_share, nodes_remainings
+        c.append((node[i]['CPUCapacity'], node[i]['MemCapacity']))
+    return c
 
 def maxMinCalculation(func, clusterCPUAvailable, clusterMemAvailable):
     fairnessDataList_CPU = []
@@ -122,8 +79,8 @@ def maxMinCalculation(func, clusterCPUAvailable, clusterMemAvailable):
     return maxMinCPUValue, maxMinMemValue
 
 
-def maxmin_Models(func, l, n, m, node_1, node_2):
-    a, nr, c, resource_needed, dominant_resource, total_resource, required_uniform_share, nodes_remainings = workload_Init(func, l, n, m, node_1, node_2)
+def maxmin_Models(func, l, n, m, node):
+    c = workload_Init(l, node)
 
     c_cpu_total = 0
     c_mem_total = 0
